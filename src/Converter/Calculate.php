ab594e6fd420b2b9f8dd3d87897bf5a05a86cbd2
Diego Brocanelli <diegod2@msn.com>
2020-06-13 08:14:04 -0300

    Implementação inicial do porjeto.

---
+++ src/Converter/Calculate.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace BNW\Converter;
+
+/**
+ * @author Diego Brocanelli <diego.brocanelli@buenonetworks.com.br>
+ */
+class Calculate
+{
+    /**
+     * Responsible for calculating the value for minute
+     * 
+     * @example$value = '34'; return 20
+     *
+     * @param  string $value
+     * @return integer
+     */
+    public function toMinutes(string $value) : int
+    {
+        $value = "0.{$value}";
+        $value = (float)$value;
+        $value = round($value * 60);
+
+        $result = (int)$value;
+
+        return $result;
+    }
+}
---
+++ src/Converter/Formater.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace BNW\Converter;
+
+/**
+ * @author Diego Brocanelli <diego.brocanelli@buenonetworks.com.br>
+ */
+class Formater
+{
+    /**
+     * Responsible for formatting the result for hours only returns.
+     *
+     * @param  string $value
+     * @return string
+     */
+    public function toHours( string $value) : string
+    {
+        return "{$value}:00";
+    }
+
+    /**
+     * Responsible for formatting the hour and minute return.
+     *
+     * @param  string $hours
+     * @param  string $minutes
+     * @return string
+     */
+    public function toTime(string $hours, string $minutes) : string
+    {
+        return "{$hours}:{$minutes}";
+    }
+
+    /**
+     * Responsible for formatting the value with the minimum number of digits.
+     * 
+     * @param  string $value
+     * @return string
+     */
+    public function strPadLeft( string $value, int $digits) : string
+    {
+        return str_pad( $value, $digits, '0', STR_PAD_LEFT );
+    }
+}
